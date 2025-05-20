13560969a3937cfb1dc29de671d45bafb5fbe30b
unknown <admin@MAPS.AT>
2025-05-20 09:52:35 +0200

    Created

---
+++ PSScripts/Script12.ps1
@@ -0,0 +1,28 @@
+﻿
+class HostInfo {
+
+    [string]$Hostname
+    [int]$RAM
+    [string]$OS
+
+    [datetime]$Created
+
+    HostInfo () {
+        $this.Created = Get-Date
+    }
+
+    HostInfo($hostname,$ram,$os) {
+        $this.Hostname = $hostname
+        $this.RAM = $ram
+        $this.OS = $os
+        $this.Created = Get-Date
+    }
+}
+
+$myHosts = @()
+
+$myHosts += [HostInfo]::new('Asterix',12,'Windows Server 2025')
+$myHosts += [HostInfo]::new('Obelix',24,'Windows Server 2016')
+$myHosts += [HostInfo]::new('Majestix',24,'Windows Server 2025')
+
+$myHosts
