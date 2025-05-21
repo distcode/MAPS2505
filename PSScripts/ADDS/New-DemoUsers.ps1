bcc7239993e5642687035f127dde4f9b8341607e
distcode <office@dist.at>
2025-05-21 16:43:11 +0200

    Created

---
+++ PSScripts/ADDS/New-DemoUsers.ps1
@@ -0,0 +1,26 @@
+﻿
+$OU = 'OU=DemoUsers,dc=maps,dc=at'
+$pwrd = ConvertTo-SecureString -String 'Pa55w.rd' -AsPlainText -Force
+
+
+for ( $i = 21 ; $i -le 20000; $i++ ) {
+
+  $GivenName = 'Rudi' + $i.ToString().PadLeft(5,'0')
+  $SurName = 'Tester'
+  $upn = "$GivenName@maps.at"
+  
+  New-ADUser -Path $OU `
+    -Name "$Givenname $SurName" `
+    -GivenName $GivenName `
+    -Surname $SurName `
+    -AccountPassword $pwrd `
+    -Enabled $true `
+    -UserPrincipalName $upn `
+    -SamAccountName $GivenName
+}
+
+$Users = Get-ADUser -Filter * -SearchBase 'ou=demousers,dc=maps,dc=at' -ResultSetSize 5500
+
+foreach ( $user in $Users) {
+  Add-ADGroupMember -Identity LocalDetectives -Members $user
+}
